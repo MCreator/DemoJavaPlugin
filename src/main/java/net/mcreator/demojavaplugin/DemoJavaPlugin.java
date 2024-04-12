e1eebba3b0a0702c0c75dea78bc92c385655441f
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2024-04-12 17:49:07 +0200

    Toolbar demo

--- src/main/java/net/mcreator/demojavaplugin/DemoJavaPlugin.java
+++ src/main/java/net/mcreator/demojavaplugin/DemoJavaPlugin.java
@@ -3,7 +3,9 @@ package net.mcreator.demojavaplugin;
 import net.mcreator.plugin.JavaPlugin;
 import net.mcreator.plugin.Plugin;
 import net.mcreator.plugin.events.workspace.MCreatorLoadedEvent;
+import net.mcreator.ui.action.BasicAction;
 import net.mcreator.ui.init.L10N;
+import net.mcreator.ui.init.UIRES;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,15 +18,18 @@ public class DemoJavaPlugin extends JavaPlugin {
     public DemoJavaPlugin(Plugin plugin) {
         super(plugin);
 
-        addListener(MCreatorLoadedEvent.class, event -> {
-            JMenu menu = new JMenu(L10N.t("plugin.demojava.menu.main"));
+        addListener(MCreatorLoadedEvent.class, event -> SwingUtilities.invokeLater(() -> {
+            BasicAction demoAction = new BasicAction(event.getMCreator().getActionRegistry(),
+                    L10N.t("plugin.demojava.menu.button"),
+                    e -> event.getMCreator().getActionRegistry().buildWorkspace.doAction());
+            demoAction.setIcon(UIRES.get("16px.play"));
 
-            JMenuItem menuItem = new JMenuItem(L10N.t("plugin.demojava.menu.button"));
-            menuItem.addActionListener(e -> event.getMCreator().getActionRegistry().buildWorkspace.doAction());
-            menu.add(menuItem);
+            JMenu menu = new JMenu(L10N.t("plugin.demojava.menu.main"));
+            menu.add(demoAction);
 
             event.getMCreator().getMainMenuBar().add(menu);
-        });
+            event.getMCreator().getToolBar().addToRightToolbar(demoAction);
+        }));
 
         LOG.info("Demo java plugin was loaded");
     }
